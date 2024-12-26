00318ee2c61cd1cf9e0e9d17ae45d4c91ed3ec19
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2024-12-26 18:48:24 +0530

    Add files via upload
    program to substract two number

---
+++ substraction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, sub;
+    cout << "enter the first number: ";
+    cin>>num1;
+    cout << "enter the second: ";
+    cin >> num2;
+    sub = num1 - num2;
+    cout << "the substration of two number = " << sub;
+    return 0;
+}  
