be667e2ba29ce10d85d34ecddf0d323d1b2146d2
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2024-12-27 18:25:46 +0530

    Add files via upload
    program to check even or odd number

---
+++ even_odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter an interger: ";
+    cin >> n;
+    if (n%2== 0)
+    {
+        cout<<"even number";
+    }
+    else
+    {
+        cout <<"odd number";
+
+    }
+    
+}
