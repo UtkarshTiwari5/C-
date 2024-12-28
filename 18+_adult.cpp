e2a77afcc981e4ab7e96fb8705281841e16836e0
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2024-12-28 19:37:02 +0530

    Add files via upload
    program to check whether the people is adult or minor through age in c++

---
+++ 18+_adult.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter age :";
+    cin>>age;
+    if (age>18)
+    {
+        cout<<"adult";
+    }
+    else
+    {
+        cout<<"minor";
+    }
+}
+
+    
+    
+
