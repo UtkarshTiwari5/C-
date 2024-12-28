d687eb695b1301847a2267e050adf4f01a847dca
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2024-12-28 19:24:26 +0530

    Add files via upload
    program to square a number in c++

---
+++ square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a mnumber:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" square is :"<<i*i<<endl;
+    }
+}
