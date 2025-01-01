4baa6371a8e3e91edf74c374b48ed77424267aa2
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2025-01-01 09:54:52 +0530

    Add files via upload
    program to calculate the area of rectangle in cpp

---
+++ areaofrectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double length, width ,area ;
+    cout<< " enter the length of rectangle : ";
+    cin >> length;
+    cout << " enter the width of rectangle : ";
+    cin >> width;
+    area = length * width;
+    cout << " area of rectangle is : "<< area << endl;
+
+
+}
