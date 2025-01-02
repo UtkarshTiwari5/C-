006489a4c246505178234ae06f287d5b8dff1714
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2025-01-02 10:00:08 +0530

    Add files via upload
    program to print the sum of first ten natural number in cpp

---
+++ sumtennaturalnumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum = 0;
+    for(int i=1; i<=10;i++)
+    {
+        sum +=i;
+    }
+    cout<< " the sum of the first ten natural number is : "<< sum << endl;
+
+}
