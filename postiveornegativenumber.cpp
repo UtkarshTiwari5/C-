30b6f2ebf594095e6ad84d79d73d7aa505302c97
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2025-01-03 21:22:28 +0530

    Add files via upload
    program to check whether the number is positive or negative in cpp

---
+++ postiveornegativenumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float number ;
+    cout << " enter a number : ";
+    cin >> number;
+    if ( number >= 0 ){
+        cout << " is a positive number " << endl;
+    }
+    else 
+    {
+        cout << " is a negative number " << endl;
+    }
+}
+
+    
+
+    
