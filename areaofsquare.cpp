98e0805eff9c1d594c9e952cf0670c6b945946f9
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2025-01-03 11:33:44 +0530

    Add files via upload
    program to calculate the area of square in cpp

---
+++ areaofsquare.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+using namespace std;
+int main()
+{
+    float side , area ;
+    cout << " enter the length of the side of the square : ";
+    cin >> side ;
+    area = side * side;
+    cout << " the area of the square is : " << area << endl;
+}
